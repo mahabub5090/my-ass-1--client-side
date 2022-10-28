e48baf60d8972c5620ec41db8a0f4843356d4041
mahabub5090 <mdmahabub.rahman789@gmail.com>
2022-10-28 23:48:31 +0600

    UI degsin updated

--- src/pages/shared/Home.js
+++ src/pages/shared/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="">
         <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900">
           <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-900">
-            Web Technology 
+            <i>Web Technology </i>
           </h1>
           <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-800">
             A best Platfrom for Learning ...
